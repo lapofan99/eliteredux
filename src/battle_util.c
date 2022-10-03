4b4bece08c6eaa617bff9752d13f6cdccc4727d0
Joël <joel@staub-it.ch>
2022-10-03 22:49:17 +0200

    Changed Abilities Teravolt & Turboblaze + changed some text
    // Teravolt
    Now also adds Electric-type
    
    // Turboblaze
    Now also adds Fire-type
    
    // Text changes
    Reworked the text of all abilities

--- src/battle_util.c
+++ src/battle_util.c
@@ -4695,6 +4695,30 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
 				effect++;
             }
             break;
+		case ABILITY_TERAVOLT:
+            if (!gSpecialStatuses[battler].switchInAbilityDone)
+            {
+                //gSpecialStatuses[battler].switchInAbilityDone = TRUE;
+				gBattleScripting.abilityPopupOverwrite = ABILITY_TERAVOLT;
+				gLastUsedAbility = ABILITY_TERAVOLT;
+				gBattleMons[battler].type3 = TYPE_ELECTRIC;
+				PREPARE_TYPE_BUFFER(gBattleTextBuff1, gBattleMons[battler].type3);
+				BattleScriptPushCursorAndCallback(BattleScript_BattlerAddedTheType);
+				effect++;
+            }
+            break;
+		case ABILITY_TURBOBLAZE:
+            if (!gSpecialStatuses[battler].switchInAbilityDone)
+            {
+                //gSpecialStatuses[battler].switchInAbilityDone = TRUE;
+				gBattleScripting.abilityPopupOverwrite = ABILITY_TURBOBLAZE;
+				gLastUsedAbility = ABILITY_TURBOBLAZE;
+				gBattleMons[battler].type3 = TYPE_FIRE;
+				PREPARE_TYPE_BUFFER(gBattleTextBuff1, gBattleMons[battler].type3);
+				BattleScriptPushCursorAndCallback(BattleScript_BattlerAddedTheType);
+				effect++;
+            }
+            break;
 		case ABILITY_COIL_UP:
             if (!gSpecialStatuses[battler].switchInAbilityDone)
             {
@@ -4855,6 +4879,32 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
 				gBattleScripting.abilityPopupOverwrite = ABILITY_DRAGONFLY;
 				gLastUsedAbility = ABILITY_DRAGONFLY;
 				gBattleMons[battler].type3 = TYPE_DRAGON;
+				PREPARE_TYPE_BUFFER(gBattleTextBuff1, gBattleMons[battler].type3);
+				BattleScriptPushCursorAndCallback(BattleScript_BattlerAddedTheType);
+				effect++;
+			}
+		}
+		// Teravolt
+		if(SpeciesHasInnate(gBattleMons[battler].species, ABILITY_TERAVOLT)){
+			if (!gSpecialStatuses[battler].switchInAbilityDone)
+			{
+				gSpecialStatuses[battler].switchInAbilityDone = TRUE;
+				gBattleScripting.abilityPopupOverwrite = ABILITY_TERAVOLT;
+				gLastUsedAbility = ABILITY_TERAVOLT;
+				gBattleMons[battler].type3 = TYPE_ELECTRIC;
+				PREPARE_TYPE_BUFFER(gBattleTextBuff1, gBattleMons[battler].type3);
+				BattleScriptPushCursorAndCallback(BattleScript_BattlerAddedTheType);
+				effect++;
+			}
+		}
+		// Turboblaze
+		if(SpeciesHasInnate(gBattleMons[battler].species, ABILITY_TURBOBLAZE)){
+			if (!gSpecialStatuses[battler].switchInAbilityDone)
+			{
+				gSpecialStatuses[battler].switchInAbilityDone = TRUE;
+				gBattleScripting.abilityPopupOverwrite = ABILITY_TURBOBLAZE;
+				gLastUsedAbility = ABILITY_TURBOBLAZE;
+				gBattleMons[battler].type3 = TYPE_FIRE;
 				PREPARE_TYPE_BUFFER(gBattleTextBuff1, gBattleMons[battler].type3);
 				BattleScriptPushCursorAndCallback(BattleScript_BattlerAddedTheType);
 				effect++;
