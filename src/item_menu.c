fdd17d7182441d83d32352290fcd75e41b3d7772
BelialClover <67656448+BelialClover@users.noreply.github.com>
2023-01-01 13:48:52 -0600

    Fixed Bag skipping the wrong pockets(it is supposed to skip the TM pocket)

--- src/item_menu.c
+++ src/item_menu.c
@@ -1325,7 +1325,8 @@ static u8 GetSwitchBagPocketDirection(void)
 
 static void ChangeBagPocketId(u8 *bagPocketId, s8 deltaBagPocketId)
 {
-    if ((deltaBagPocketId == MENU_CURSOR_DELTA_RIGHT && *bagPocketId == BALLS_POCKET) || (deltaBagPocketId == MENU_CURSOR_DELTA_LEFT && *bagPocketId == BERRIES_POCKET))
+    if ((deltaBagPocketId == MENU_CURSOR_DELTA_RIGHT && *bagPocketId == BATTLE_POCKET) || 
+        (deltaBagPocketId == MENU_CURSOR_DELTA_LEFT  && *bagPocketId == BERRIES_POCKET))
         *bagPocketId += deltaBagPocketId*2;
     else if (deltaBagPocketId == MENU_CURSOR_DELTA_RIGHT && *bagPocketId == POCKETS_COUNT - 1)
         *bagPocketId = 0;
