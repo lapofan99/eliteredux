60289c284c281588050d0dc61d2d68e5aa9ef131
BelialClover <67656448+BelialClover@users.noreply.github.com>
2023-04-24 17:06:50 -0500

    other fix

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -7764,7 +7764,7 @@ BattleScript_BattlerEnvelopedItselfInAVeil::
 BattleScript_AirBlowerActivated::
 	copybyte gBattlerAbility, gBattlerAttacker
 	sethword sABILITY_OVERWRITE, ABILITY_AIR_BLOWER
-	showabilitypopup BS_TARGET
+	showabilitypopup BS_ABILITY_BATTLER
 	printstring STRINGID_AIRBLOWERACTIVATED
 	waitmessage B_WAIT_TIME_LONG
 	sethword sABILITY_OVERWRITE, 0
