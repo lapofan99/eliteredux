eb956c4a585de853badbe4a832b63113129dfe5f
BelialClover <67656448+BelialClover@users.noreply.github.com>
2023-05-16 13:49:42 -0500

    Revert "Scrolling Start Menu"
    This reverts commit ff9cd223d359ee1c7bbdd32bae84f0a1bf1f2686.

--- src/start_menu.c
+++ src/start_menu.c
@@ -70,11 +70,7 @@ enum
 	MENU_ACTION_DEXNAV,
     MENU_ACTION_ACCESS_PC,
 	MENU_ACTION_DEBUG,
-    MENU_ACTION_NUM,
 };
-
-#define StartMenu_ActionNum 8
-#define StartMenu_maxShowed 8
 
 // Save status
 enum
@@ -93,8 +89,7 @@ EWRAM_DATA static u8 sSafariBallsWindowId = 0;
 EWRAM_DATA static u8 sBattlePyramidFloorWindowId = 0;
 EWRAM_DATA static u8 sStartMenuCursorPos = 0;
 EWRAM_DATA static u8 sNumStartMenuActions = 0;
-EWRAM_DATA static u8 sCurrentStartMenuActions[StartMenu_ActionNum] = {0};
-EWRAM_DATA static u8 startMenuTaskId = 0;
+EWRAM_DATA static u8 sCurrentStartMenuActions[9] = {0};
 EWRAM_DATA static u8 sInitStartMenuData[2] = {0};
 
 EWRAM_DATA static u8 (*sSaveDialogCallback)(void) = NULL;
@@ -386,7 +381,7 @@ static void BuildNormalStartMenu(void)
 
     if(!DisableSave)
 	    AddStartMenuAction(MENU_ACTION_ACCESS_PC);
-    AddStartMenuAction(MENU_ACTION_EXIT);
+    //AddStartMenuAction(MENU_ACTION_EXIT);
 }
 
 static void BuildDebugStartMenu(void)
@@ -528,19 +523,16 @@ static bool32 PrintStartMenuActions(s8 *pIndex, u32 count)
 {
     s8 index = *pIndex;
 
-    struct ListMenuItem *tempItems = (void*)&gStringVar4[0x200];
-    gMultiuseListMenuTemplate.items=tempItems;
     do
     {
         if (sStartMenuItems[sCurrentStartMenuActions[index]].func.u8_void == StartMenuPlayerNameCallback)
         {
-            tempItems[index].name = gSaveBlock2Ptr->playerName;
-            tempItems[index].id = index;
+            PrintPlayerNameOnWindow(GetStartMenuWindowId(), sStartMenuItems[sCurrentStartMenuActions[index]].text, 8, (index << 4) + 9);
         }
         else
         {
-            tempItems[index].name = sStartMenuItems[sCurrentStartMenuActions[index]].text;
-            tempItems[index].id = index;
+            StringExpandPlaceholders(gStringVar4, sStartMenuItems[sCurrentStartMenuActions[index]].text);
+            AddTextPrinterParameterized(GetStartMenuWindowId(), 1, gStringVar4, 8, (index << 4) + 9, 0xFF, NULL);
         }
 
         index++;
@@ -558,29 +550,6 @@ static bool32 PrintStartMenuActions(s8 *pIndex, u32 count)
     return FALSE;
 }
 
-#include "list_menu.h"
-static const struct ListMenuTemplate sCommonListMenuTemplate =
-{
-    .items = 0,
-    .moveCursorFunc = 0,
-    .itemPrintFunc = 0,
-    .totalItems = 0,
-    .maxShowed = StartMenu_ActionNum,
-    .windowId = 0,
-    .header_X = 0,
-    .item_X = 8,
-    .cursor_X = 0,
-    .upText_Y = 1,
-    .cursorPal = 2,
-    .fillValue = 1,
-    .cursorShadowPal = 3,
-    .lettersSpacing = 1,
-    .itemVerticalPadding = 0,
-    .scrollMultiple = 0,
-    .fontId = 1,
-    .cursorKind = 0
-};
-
 static bool32 InitStartMenuStep(void)
 {
     s8 state = sInitStartMenuData[0];
@@ -596,10 +565,7 @@ static bool32 InitStartMenuStep(void)
         break;
     case 2:
         LoadMessageBoxAndBorderGfx();
-        if (sNumStartMenuActions < StartMenu_ActionNum)
-            DrawStdWindowFrame(AddStartMenuWindow(sNumStartMenuActions), FALSE);
-        else
-            DrawStdWindowFrame(AddStartMenuWindow(7), FALSE);
+        DrawStdWindowFrame(sub_81979C4(sNumStartMenuActions), FALSE);
         sInitStartMenuData[1] = 0;
         sInitStartMenuData[0]++;
         break;
@@ -611,20 +577,11 @@ static bool32 InitStartMenuStep(void)
         sInitStartMenuData[0]++;
         break;
     case 4:
-        gMultiuseListMenuTemplate = sCommonListMenuTemplate;
-        gMultiuseListMenuTemplate.windowId = GetStartMenuWindowId();
-        if (PrintStartMenuActions(&sInitStartMenuData[1], 3))
-        {
-            gMultiuseListMenuTemplate.totalItems = sNumStartMenuActions;
-            //If you add a new option and find bug, please modify here
-            if(sStartMenuCursorPos < 5)
-                gTasks[startMenuTaskId].data[5] = ListMenuInit(&gMultiuseListMenuTemplate, 0, sStartMenuCursorPos);
-            else
-                gTasks[startMenuTaskId].data[5] = ListMenuInit(&gMultiuseListMenuTemplate, 1, sStartMenuCursorPos - 1);
+        if (PrintStartMenuActions(&sInitStartMenuData[1], 2))
             sInitStartMenuData[0]++;
-        }
         break;
     case 5:
+        sStartMenuCursorPos = sub_81983AC(GetStartMenuWindowId(), 1, 0, 9, 16, sNumStartMenuActions, sStartMenuCursorPos);
         CopyWindowToVram(GetStartMenuWindowId(), TRUE);
         return TRUE;
     }
@@ -654,7 +611,6 @@ static void CreateStartMenuTask(TaskFunc followupFunc)
     sInitStartMenuData[1] = 0;
     taskId = CreateTask(StartMenuTask, 0x50);
     SetTaskFuncWithFollowupFunc(taskId, StartMenuTask, followupFunc);
-    startMenuTaskId = taskId;
 }
 
 static bool8 FieldCB_ReturnToFieldStartMenu(void)
@@ -709,11 +665,20 @@ void ShowStartMenu(void)
 
 static bool8 HandleStartMenuInput(void)
 {
-    s32 input = ListMenu_ProcessInput(gTasks[startMenuTaskId].data[5]);
-
-    if (input >= 0)
+    if (JOY_NEW(DPAD_UP))
     {
-        sStartMenuCursorPos = input;
+        PlaySE(SE_SELECT);
+        sStartMenuCursorPos = Menu_MoveCursor(-1);
+    }
+
+    if (JOY_NEW(DPAD_DOWN))
+    {
+        PlaySE(SE_SELECT);
+        sStartMenuCursorPos = Menu_MoveCursor(1);
+    }
+
+    if (JOY_NEW(A_BUTTON))
+    {
         PlaySE(SE_SELECT);
         if (sStartMenuItems[sCurrentStartMenuActions[sStartMenuCursorPos]].func.u8_void == StartMenuPokedexCallback)
         {
@@ -1181,7 +1146,7 @@ static u8 SaveFileExistsCallback(void)
     }
     else
     {
-        sSaveDialogCallback = SaveSavingMessageCallback;
+        ShowSaveMessage(gText_AlreadySavedFile, SaveConfirmOverwriteCallback);
     }
 
     return SAVE_IN_PROGRESS;
@@ -1569,7 +1534,6 @@ void SaveForBattleTowerLink(void)
 static void HideStartMenuWindow(void)
 {
     ClearStdWindowAndFrame(GetStartMenuWindowId(), TRUE);
-    DestroyListMenuTask(gTasks[startMenuTaskId].data[5], 0 ,0);
     RemoveStartMenuWindow();
     ScriptUnfreezeObjectEvents();
     ScriptContext2_Disable();
