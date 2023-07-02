2c344c10db99dd4b2512bedd4b1be1590f6a6a9c
BelialClover <67656448+BelialClover@users.noreply.github.com>
2023-07-02 13:35:15 -0500

    Disabled Soul Linker working against another Soul Linker mon

--- src/battle_util.c
+++ src/battle_util.c
@@ -6901,6 +6901,8 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
               && gBattleMons[gBattlerTarget].hp != 0
               && !gProtectStructs[gBattlerAttacker].confusionSelfDmg
               && move != MOVE_PAIN_SPLIT
+              && gBattleMons[gBattlerTarget].ability != ABILITY_SOUL_LINKER
+              && !BattlerHasInnate(gBattlerTarget, ABILITY_SOUL_LINKER)
               && TARGET_TURN_DAMAGED)
             {
                 BattleScriptPushCursor();
@@ -7283,10 +7285,11 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
               && gBattleMons[gBattlerTarget].hp != 0
               && !gProtectStructs[gBattlerAttacker].confusionSelfDmg
               && move != MOVE_PAIN_SPLIT
+              && gBattleMons[gBattlerTarget].ability != ABILITY_SOUL_LINKER
+              && !BattlerHasInnate(gBattlerTarget, ABILITY_SOUL_LINKER)
               && TARGET_TURN_DAMAGED)
             {
-                gBattleScripting.abilityPopupOverwrite = ABILITY_SOUL_LINKER;
-				gLastUsedAbility = ABILITY_SOUL_LINKER;
+                gBattleScripting.abilityPopupOverwrite = gLastUsedAbility = ABILITY_SOUL_LINKER;
                 BattleScriptPushCursor();
                 gBattlescriptCurrInstr = BattleScript_AttackerSoulLinker;
                 effect++;
@@ -7845,6 +7848,8 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
             if (!(gMoveResultFlags & MOVE_RESULT_NO_EFFECT)
               && gBattleMons[gBattlerTarget].hp != 0
               && !gProtectStructs[gBattlerAttacker].confusionSelfDmg
+              && gBattleMons[gBattlerTarget].ability != ABILITY_SOUL_LINKER
+              && !BattlerHasInnate(gBattlerTarget, ABILITY_SOUL_LINKER)
               && move != MOVE_PAIN_SPLIT
               && TARGET_TURN_DAMAGED)
             {
@@ -7919,6 +7924,8 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
             if (!(gMoveResultFlags & MOVE_RESULT_NO_EFFECT)
               && gBattleMons[gBattlerTarget].hp != 0
               && !gProtectStructs[gBattlerAttacker].confusionSelfDmg
+              && gBattleMons[gBattlerTarget].ability != ABILITY_SOUL_LINKER
+              && !BattlerHasInnate(gBattlerTarget, ABILITY_SOUL_LINKER)
               && move != MOVE_PAIN_SPLIT
               && TARGET_TURN_DAMAGED)
             {
