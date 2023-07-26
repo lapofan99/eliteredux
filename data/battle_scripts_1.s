c67db8a6e763b55b6c61ac077fbb04b0ed89d653
BelialClover <67656448+BelialClover@users.noreply.github.com>
2023-07-26 18:14:36 -0500

    Removed Recoil Damage from mons with impenetrable and magic guard

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -7793,8 +7793,10 @@ BattleScript_MoveEffectRecoilWithStatus::
 	argumentstatuseffect
 BattleScript_MoveEffectRecoil::
 	jumpifmove MOVE_STRUGGLE, BattleScript_DoRecoil
-	jumpifability BS_ATTACKER, ABILITY_ROCK_HEAD, BattleScript_RecoilEnd
-	jumpifability BS_ATTACKER, ABILITY_BAD_COMPANY, BattleScript_RecoilEnd
+	jumpifability BS_ATTACKER, ABILITY_ROCK_HEAD,    BattleScript_RecoilEnd
+	jumpifability BS_ATTACKER, ABILITY_IMPENETRABLE, BattleScript_RecoilEnd
+	jumpifability BS_ATTACKER, ABILITY_MAGIC_GUARD,  BattleScript_RecoilEnd
+	jumpifability BS_ATTACKER, ABILITY_BAD_COMPANY,  BattleScript_RecoilEnd
 BattleScript_DoRecoil::
 	orword gHitMarker, HITMARKER_IGNORE_SUBSTITUTE | HITMARKER_PASSIVE_DAMAGE | HITMARKER_IGNORE_DISGUISE
 	healthbarupdate BS_ATTACKER
