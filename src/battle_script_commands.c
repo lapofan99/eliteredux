5b3114ae22158577702717e6400efb880f0ccf6c
mutantmassadri <105794109+mutantmassadri@users.noreply.github.com>
2023-08-09 09:08:23 +0200

    Flame Burst ally damage from 1/16 to 1/4th

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3569,7 +3569,7 @@ void SetMoveEffect(bool32 primary, u32 certain)
 		            !BattlerHasInnate(BATTLE_PARTNER(gBattlerTarget), ABILITY_IMPENETRABLE))
                 {
                     gBattleScripting.savedBattler = BATTLE_PARTNER(gBattlerTarget);
-                    gBattleMoveDamage = gBattleMons[BATTLE_PARTNER(gBattlerTarget)].hp / 16;
+                    gBattleMoveDamage = gBattleMons[BATTLE_PARTNER(gBattlerTarget)].hp / 4;
                     if (gBattleMoveDamage == 0)
                         gBattleMoveDamage = 1;
                     gBattlescriptCurrInstr = BattleScript_MoveEffectFlameBurst;
