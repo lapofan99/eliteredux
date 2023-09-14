d08bd17bba3b92b9baa2931672bef3b5082d85ff
Joël <joel@staub-it.ch>
2023-09-14 21:47:23 +0200

    Update abilities.h

--- include/constants/abilities.h
+++ include/constants/abilities.h
@@ -435,13 +435,13 @@
 #define ABILITY_LOOSE_ROCKS      (ABILITIES_COUNT_LATEST_GEN + 137) // when being hit by any attacking move (like Stamina), trigger Stealth Rock
 #define ABILITY_SPINNING_TOP     (ABILITIES_COUNT_LATEST_GEN + 138) // Fighting-type moves raise speed by +1 and clear hazards.
 #define ABILITY_RETRIBUTION_BLOW (ABILITIES_COUNT_LATEST_GEN + 139) // Everytime a foe uses a stat boosting move, trigger a full power Hyperbeam
-#define ABILITY_FEARMONGER       (ABILITIES_COUNT_LATEST_GEN + 140) // Scare + Intimidate + 10% chance to paralyze with attacks
-#define ABILITY_KINGS_WRATH      (ABILITIES_COUNT_LATEST_GEN + 141) // When a stat is lowered, gain +1 Atk and Def. Includes Ally Drops.
+#define ABILITY_FEARMONGER       (ABILITIES_COUNT_LATEST_GEN + 140) // Scare + Intimidate + 10% chance to paralyze with attacks.
+#define ABILITY_KINGS_WRATH      (ABILITIES_COUNT_LATEST_GEN + 141) // When a stat is lowered, boost Atk and Def by +1. Includes Ally Drops.
 #define ABILITY_QUEENS_MOURNING  (ABILITIES_COUNT_LATEST_GEN + 142) // When a stat is lowered, boost SpAtk and SpDef by +1. Includes Ally Drops.
-#define ABILITY_TOXIC_SPILL      (ABILITIES_COUNT_LATEST_GEN + 143) // The user regurgitates it's toxic fluids onto the field, causing all non-Poison Type Pokemon on the field to take 1/8th of their max HP at the end of every turn.
-#define ABILITY_DESERT_CLOAK     (ABILITIES_COUNT_LATEST_GEN + 144) // The pokemon shrouds itself and its allies protecting them from status conditions and secondary effects while under sand.
+#define ABILITY_TOXIC_SPILL      (ABILITIES_COUNT_LATEST_GEN + 143) // The user regurgitates it's toxic fluids onto the field, causing all non-Poison Type Pokemon on the field to take 1/8th of their max HP at the end of every turn. Effect disappears when the Pokémon is not on the field anymore.
+#define ABILITY_DESERT_CLOAK     (ABILITIES_COUNT_LATEST_GEN + 144) // The pokemon shrouds itself and allies protecting them from status conditions and secondary effects while under sand.
 #define ABILITY_DRACONIZE        (ABILITIES_COUNT_LATEST_GEN + 145) // All Normal-type moves used by the Pokémon become Dragon-type and receive a 20% power boost.
-#define ABILITY_PRETTY_PRINCESS  (ABILITIES_COUNT_LATEST_GEN + 146) // +50% DMG against foes with lowered stats
+#define ABILITY_PRETTY_PRINCESS  (ABILITIES_COUNT_LATEST_GEN + 146) // Increases the damage dealt against foes with lowered stats by 50%.
 #define ABILITY_SELF_REPAIR      (ABILITIES_COUNT_LATEST_GEN + 147) // Self Sufficient + Natural Cure
 #define ABILITY_ATOMIC_BURST     (ABILITIES_COUNT_LATEST_GEN + 148) // When taking super-effective damage, use Hyper Beam at 33% Power.
 #define ABILITY_HELLBLAZE        (ABILITIES_COUNT_LATEST_GEN + 149) // Ups Fire moves by 30%, 80% when at 1/3 HP.
