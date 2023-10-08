0624ecf3e28519bb645d4d2c06a4b6697fde3b18
BelialClover <67656448+BelialClover@users.noreply.github.com>
2023-10-08 17:45:56 -0600

    Shucklenite

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1304,7 +1304,7 @@
 #define FLAG_LENT_NURSE_SURF                                        0x4EC
 #define FLAG_RAN_FROM_TRAINER                                       0x4ED // Unused Flag
 #define FLAG_HANDED_OVER_METEORITE                                  0x4EE
-#define FLAG_UNUSED_0x4EF                                           0x4EF // Unused Flag
+#define FLAG_OBTAINED_SHUCKLENITE                                   0x4EF
 
 #define FLAG_DEFEATED_RUSTBORO_GYM                                  0x4F0
 #define FLAG_DEFEATED_DEWFORD_GYM                                   0x4F1
