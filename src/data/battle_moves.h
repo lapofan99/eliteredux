be72eb262bd3c64747e8ad915378229dfa31ee5e
Mawootad <adinfinatioma@gmail.com>
2023-12-23 18:27:55 -0800

    Give sheer force missing weather based and sheer force boost tags

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -5277,7 +5277,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .secondaryEffectChance = 20,
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
-        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED,
+        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_SHEER_FORCE_BOOST | FLAG_WEATHER_BASED,
         .split = SPLIT_SPECIAL,
     },
 
